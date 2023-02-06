344f84715546b2fd5ab346a8ed9e1abe554c0de9
Andrey Kudlay <matroskin8@i.ua>
2023-02-06 15:28:37 +0200

    sendMessage

--- index.php
+++ index.php
@@ -14,9 +14,18 @@ function debug($data)
     echo '<pre>' . print_r($data, true) .  '</pre>';
 }
 
-const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
-$url = BASE_URL . 'getMe';
-$res = json_decode(file_get_contents($url), true);
-debug($res);
+$url = BASE_URL . 'getUpdates';
+$res = json_decode(file_get_contents($url));
+//debug($res);
 //echo $res->result->id;
-echo $res['result']['id'];
+//echo $res['result']['id'];
+
+if (!empty($res->result)) {
+    foreach ($res->result as $update) {
+        $text = "<b><i>Вы написали:</i></b> <i>{$update->message->text}</i>";
+        echo $text . PHP_EOL;
+        $url = BASE_URL . "sendMessage?chat_id={$update->message->chat->id}&text={$text}&parse_mode=HTML";
+        file_get_contents($url);
+    }
+}
+
