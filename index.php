edcd7957c26c29150dcd8ad348a3c47caacc0003
Andrey Kudlay <matroskin8@i.ua>
2023-02-06 23:02:11 +0200

    echo bot

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-/*
-hi484690.ftp.tools
-hi484690_ftp
-1g6EYrs7f1
-*/
+set_time_limit(0);
 
-// быстрая отправка (сжать изображение)
 require_once __DIR__ . '/config.php';
 
 function debug($data)
@@ -14,18 +9,35 @@ function debug($data)
     echo '<pre>' . print_r($data, true) .  '</pre>';
 }
 
-$url = BASE_URL . 'getUpdates';
-$res = json_decode(file_get_contents($url));
-//debug($res);
-//echo $res->result->id;
-//echo $res['result']['id'];
-
-if (!empty($res->result)) {
-    foreach ($res->result as $update) {
-        $text = "<b><i>Вы написали:</i></b> <i>{$update->message->text}</i>";
-        echo $text . PHP_EOL;
-        $url = BASE_URL . "sendMessage?chat_id={$update->message->chat->id}&text={$text}&parse_mode=HTML";
-        file_get_contents($url);
+function sendRequest($method, $params = [])
+{
+    $url = BASE_URL . $method;
+    if (!empty($params)) {
+        $url .= '?' . http_build_query($params);
     }
+    return json_decode(file_get_contents($url));
 }
 
+while (true) {
+    $params = [];
+    if (isset($last_update)) {
+        $params = [
+            'offset' => $last_update + 1,
+        ];
+    }
+    $res = sendRequest('getUpdates', $params);
+
+    if (!empty($res->result)) {
+        foreach ($res->result as $update) {
+            $text = "<b><i>Вы написали:</i></b> <i>{$update->message->text}</i>";
+            echo $text . PHP_EOL;
+            $last_update = $update->update_id;
+            sendRequest('sendMessage', [
+                'chat_id' => $update->message->chat->id,
+                'text' => $text,
+                'parse_mode' => 'HTML',
+            ]);
+        }
+    }
+    sleep(3);
+}
